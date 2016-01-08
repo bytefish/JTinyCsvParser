3264f0f4ceb23c7deb78a52799148203a96d9359
Philipp Wagner <bytefish@gmx.de>
2016-01-08 18:34:02 +0100

    Adding Converters

---
+++ JTinyCsvParser/src/main/java/de/bytefish/jtinycsvparser/typeconverter/BigIntegerConverter.java
@@ -0,0 +1,30 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package de.bytefish.jtinycsvparser.typeconverter;
+
+import java.lang.reflect.Type;
+import java.math.BigInteger;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class BigIntegerConverter implements ITypeConverter<BigInteger> {
+
+    public BigIntegerConverter() {
+
+    }
+
+    @Override
+    public BigInteger convert(String value) {
+        try {
+            return new BigInteger(value);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Type getTargetType() {
+        return BigInteger.class;
+    }
+}
---
+++ JTinyCsvParser/src/main/java/de/bytefish/jtinycsvparser/typeconverter/ShortConverter.java
@@ -0,0 +1,35 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package de.bytefish.jtinycsvparser.typeconverter;
+
+import java.lang.reflect.Type;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class ShortConverter implements ITypeConverter<Short> {
+
+    private NumberFormat numberFormat;
+
+    public ShortConverter() {
+        this(NumberFormat.getInstance(Locale.US));
+    }
+
+    public ShortConverter(NumberFormat numberFormat) {
+        this.numberFormat = numberFormat;
+    }
+
+    @Override
+    public Short convert(String value) {
+        try {
+            return numberFormat.parse(value).shortValue();
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Type getTargetType() {
+        return Short.class;
+    }
+}
