10d320457e07d17c9f6a97ac2e662ba4b0f8bb00
Philipp Wagner <bytefish@gmx.de>
2016-02-04 14:43:36 +0100

    Refactor Bundle Creation
    Moving the Manual Creation of the Library creation for the Maven Central
    Repository into its own folder. It would probably be easier to use ant
    or maven for the signature / deployment steps.

--- JTinyCsvParser/deployment/create_bundle.bat
+++ JTinyCsvParser/deployment/create_bundle.bat
@@ -15,18 +15,23 @@ set GPG_EXECUTABLE="C:\Program Files (x86)\GNU\GnuPG\pub\gpg.exe"
 set STDOUT=stdout.log
 set STDERR=stderr.log
 
-:: Set the Target Bundle file:
-set TARGET_BUNDLE=bundle.jar
+:: Version to build the bundle for:
+set VERSION=1.0
 
-set FILENAME=jtinycsvparser-1.0
+:: Set the Target Bundle file:
+set TARGET_BUNDLE=bundle\jtinycsvparser-bundle-%VERSION%.jar
 
 :: Define the Artifacts to be signed. Simply use an absolute path here:
-set JAR_DIR=D:\github\JTinyCsvParser\JTinyCsvParser\out\artifacts\JTinyCsvParser_1_0
-set JAR_FILE=jtinycsvparser-1.0.jar
-set JAR_SOURCES_FILE=jtinycsvparser-1.0-sources.jar
-set JAR_JAVADOC_FILE=jtinycsvparser-1.0-javadoc.jar
+set ARTIFACTS_DIR=artifacts
+set BASE_DIR=%ARTIFACTS_DIR%\%VERSION%
 
-set POM_DIR=D:\github\JTinyCsvParser\JTinyCsvParser
+:: Set the Filename for artifacts to use:
+set FILENAME=jtinycsvparser-%VERSION%
+
+set JAR_FILE=%FILENAME%.jar
+set JAR_SOURCES_FILE=%FILENAME%-sources.jar
+set JAR_JAVADOC_FILE=%FILENAME%-javadoc.jar
+
 set POM_FILE=pom.xml
 
 set JAR_FILE_ASC=%JAR_FILE%.asc
@@ -41,16 +46,17 @@ set /p PASSPHRASE="Signing Passphrase: "
 1>%STDOUT% 2>%STDERR% (
     
     :: Create Fake:
-    %JAR_EXECUTABLE% -cf %JAR_SOURCES_FILE% README.txt
-    %JAR_EXECUTABLE% -cf %JAR_JAVADOC_FILE% README.txt
+    %JAR_EXECUTABLE% -cf %BASE_DIR%\%JAR_SOURCES_FILE% -C %ARTIFACTS_DIR% README.txt
+    %JAR_EXECUTABLE% -cf %BASE_DIR%\%JAR_JAVADOC_FILE% -C %ARTIFACTS_DIR% README.txt
     
     :: Sign JAR and POM Files:
-    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%JAR_DIR%\%JAR_FILE%"  
-    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%POM_DIR%\%POM_FILE%"
-    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%JAR_SOURCES_FILE%"
-    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%JAR_JAVADOC_FILE%"
+    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%BASE_DIR%\%JAR_FILE%"  
+    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%BASE_DIR%\%POM_FILE%"
+    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%BASE_DIR%\%JAR_SOURCES_FILE%"
+    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%BASE_DIR%\%JAR_JAVADOC_FILE%"
 
-    %JAR_EXECUTABLE% -cf "%TARGET_BUNDLE%" -C "%JAR_DIR%" "%JAR_FILE%" -C "%JAR_DIR%" "%JAR_FILE_ASC%" -C "%POM_DIR%" "%POM_FILE%" -C "%POM_DIR%" "%POM_FILE_ASC%" -C . %JAR_SOURCES_FILE% %JAR_JAVADOC_FILE% %JAR_SOURCES_FILE_ASC% %JAR_JAVADOC_FILE_ASC% 
+    :: Create the Bundle File for manual upload:
+    %JAR_EXECUTABLE% -cf "%TARGET_BUNDLE%" -C "%BASE_DIR%" "%JAR_FILE%" -C "%BASE_DIR%" "%JAR_FILE_ASC%" -C "%BASE_DIR%" "%POM_FILE%" -C "%BASE_DIR%" "%POM_FILE_ASC%" -C "%BASE_DIR%" "%JAR_SOURCES_FILE%" -C "%BASE_DIR%" "%JAR_JAVADOC_FILE%" -C "%BASE_DIR%" "%JAR_SOURCES_FILE_ASC% -C "%BASE_DIR%" %JAR_JAVADOC_FILE_ASC%"
     
 )
 
