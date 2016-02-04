a0525a5ca24d3c6e54af8d0bfec3022a81796a85
Philipp Wagner <bytefish@gmx.de>
2016-02-04 13:33:33 +0100

    Added Fake JAR creation

--- JTinyCsvParser/deployment/create_bundle.bat
+++ JTinyCsvParser/deployment/create_bundle.bat
@@ -18,15 +18,21 @@ set STDERR=stderr.log
 :: Set the Target Bundle file:
 set TARGET_BUNDLE=bundle.jar
 
+set FILENAME=jtinycsvparser-1.0
+
 :: Define the Artifacts to be signed. Simply use an absolute path here:
 set JAR_DIR=D:\github\JTinyCsvParser\JTinyCsvParser\out\artifacts\JTinyCsvParser_1_0
 set JAR_FILE=jtinycsvparser-1.0.jar
+set JAR_SOURCES_FILE=jtinycsvparser-1.0-sources.jar
+set JAR_JAVADOC_FILE=jtinycsvparser-1.0-javadoc.jar
 
 set POM_DIR=D:\github\JTinyCsvParser\JTinyCsvParser
 set POM_FILE=pom.xml
 
 set JAR_FILE_ASC=%JAR_FILE%.asc
 set POM_FILE_ASC=%POM_FILE%.asc
+set JAR_SOURCES_FILE_ASC=%JAR_SOURCES_FILE%.asc
+set JAR_JAVADOC_FILE_ASC=%JAR_JAVADOC_FILE%.asc
 
 :: Ask for GPG Signing Passphrase:
 
@@ -34,11 +40,17 @@ set /p PASSPHRASE="Signing Passphrase: "
 
 1>%STDOUT% 2>%STDERR% (
     
+    :: Create Fake:
+    %JAR_EXECUTABLE% -cf %JAR_SOURCES_FILE% README.txt
+    %JAR_EXECUTABLE% -cf %JAR_JAVADOC_FILE% README.txt
+    
     :: Sign JAR and POM Files:
     echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%JAR_DIR%\%JAR_FILE%"  
     echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%POM_DIR%\%POM_FILE%"
+    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%JAR_SOURCES_FILE%"
+    echo %PASSPHRASE%|%GPG_EXECUTABLE% --batch --yes --passphrase-fd 0  -b -a -s "%JAR_JAVADOC_FILE%"
 
-    %JAR_EXECUTABLE% -cf "%TARGET_BUNDLE%" -C "%JAR_DIR%" "%JAR_FILE%" -C "%JAR_DIR%" "%JAR_FILE_ASC%" -C "%POM_DIR%" "%POM_FILE%" -C "%POM_DIR%" "%POM_FILE_ASC%"
+    %JAR_EXECUTABLE% -cf "%TARGET_BUNDLE%" -C "%JAR_DIR%" "%JAR_FILE%" -C "%JAR_DIR%" "%JAR_FILE_ASC%" -C "%POM_DIR%" "%POM_FILE%" -C "%POM_DIR%" "%POM_FILE_ASC%" -C . %JAR_SOURCES_FILE% %JAR_JAVADOC_FILE% %JAR_SOURCES_FILE_ASC% %JAR_JAVADOC_FILE_ASC% 
     
 )
 
